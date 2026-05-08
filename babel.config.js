230db889706b35d2fa66714111ff88b72e1953fb
shristi2076 <shristijoshi03@gmail.com>
2026-05-08 12:23:53 +0545

    feat(setup): Setup project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@assets': './src/assets',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
